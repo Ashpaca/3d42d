b10344fb2625143b9562778621c62d3204eb8673
Ashpaca <ashley.ashley.harrison@gmail.com>
2025-05-03 00:37:45 -0700

    added side textures and cube to level

--- guy.gd
+++ guy.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED : float = 5.0
+const JUMP_VELOCITY : float = 4.5
 
 
 func _physics_process(delta: float) -> void:
@@ -16,8 +16,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir : Vector2 = Input.get_vector("left", "right", "up", "down")
+	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +26,9 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	for collisionID in range(get_slide_collision_count()):
+		if get_slide_collision(collisionID).get_collider() is RigidBody3D:
+			var body : RigidBody3D = get_slide_collision(collisionID).get_collider() as RigidBody3D
+			body.apply_central_impulse((body.global_position - Vector3(global_position.x, global_position.y - .5, global_position.z)).normalized() * 2)
+			
